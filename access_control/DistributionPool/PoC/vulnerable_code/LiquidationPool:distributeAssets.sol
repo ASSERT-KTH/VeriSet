6187ff9bd5ce8382d93ae768934dc66038ab8a8a
linussve <linussve@kth.se>
2025-05-02 10:40:02 +0200

    acces-control:DistributionPool

---
+++ access_control/DistributionPool/PoC/vulnerable_code/LiquidationPool:distributeAssets.sol
@@ -0,0 +1,39 @@
+File: LiquidationPool.sol
+
+205: function distributeAssets(ILiquidationPoolManager.Asset[] memory _assets, uint256 _collateralRate, uint256 _hundredPC) external payable {
+        consolidatePendingStakes();
+        (,int256 priceEurUsd,,,) = Chainlink.AggregatorV3Interface(eurUsd).latestRoundData();
+        uint256 stakeTotal = getStakeTotal();
+        uint256 burnEuros;
+        uint256 nativePurchased;
+        for (uint256 j = 0; j < holders.length; j++) {
+            Position memory _position = positions[holders[j]];
+            uint256 _positionStake = stake(_position);
+            if (_positionStake > 0) {
+                for (uint256 i = 0; i < _assets.length; i++) {
+                    ILiquidationPoolManager.Asset memory asset = _assets[i];
+                    if (asset.amount > 0) {
+                        (,int256 assetPriceUsd,,,) = Chainlink.AggregatorV3Interface(asset.token.clAddr).latestRoundData();
+                        uint256 _portion = asset.amount * _positionStake / stakeTotal;
+                        uint256 costInEuros = _portion * 10 ** (18 - asset.token.dec) * uint256(assetPriceUsd) / uint256(priceEurUsd)
+                            * _hundredPC / _collateralRate;
+                        if (costInEuros > _position.EUROs) {
+                            _portion = _portion * _position.EUROs / costInEuros;
+                            costInEuros = _position.EUROs;
+                        }
+                        _position.EUROs -= costInEuros;
+                        rewards[abi.encodePacked(_position.holder, asset.token.symbol)] += _portion;
+                        burnEuros += costInEuros;
+                        if (asset.token.addr == address(0)) {
+                            nativePurchased += _portion;
+                        } else {
+                            IERC20(asset.token.addr).safeTransferFrom(manager, address(this), _portion);
+                        }
+                    }
+                }
+            }
+            positions[holders[j]] = _position;
+        }
+        if (burnEuros > 0) IEUROs(EUROs).burn(address(this), burnEuros);
+        returnUnpurchasedNative(_assets, nativePurchased);
+    }
